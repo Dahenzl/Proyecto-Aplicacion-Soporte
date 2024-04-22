4d11f7bc024a0f52fe04cd28f8aa8e6c1f3b9a1b
David <ddhenriquez@uninorte.edu.co>
2024-04-21 21:09:29 -0500

    Creacion pagina de login y esquema base del proyectp

---
+++ lib/ui/controller/authentication_controller.dart
@@ -0,0 +1,33 @@
+import 'package:get/get.dart';
+import 'package:loggy/loggy.dart';
+
+class AuthenticationController extends GetxController {
+  final logged = false.obs;
+  final errorMessage = ''.obs; // Mensaje de error observable
+
+  bool get isLogged => logged.value;
+
+  Future<void> login(String email, String password) async {
+    try {
+      // Aquí deberías agregar la lógica real de inicio de sesión, como llamar a una API para autenticar al usuario.
+      // Este es un ejemplo de simulación de inicio de sesión exitoso basado en las credenciales del usuario.
+      if (email == 'a@a.com' || email == 'b@b.com' && password == '123456') {
+        logged.value = true;
+        errorMessage.value = ''; // Limpiar mensaje de error
+      } else if (email == 'c@c.com ' && password == '123456')  {
+        logged.value = true;
+        errorMessage.value = ''; // Limpiar mensaje de error
+      } else {
+        throw Exception('Credenciales inválidas');
+      }
+    } catch (e) {
+      logError('Error en el inicio de sesión: $e');
+      errorMessage.value = 'Error al iniciar sesión'; // Establecer mensaje de error
+      throw e;
+    }
+  }
+
+  Future<void> logOut() async {
+    logged.value = false;
+  }
+}
